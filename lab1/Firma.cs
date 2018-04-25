518a371c35980e5b03e3a960216bb2ebe6f93a34
alexandra1488228 <ivasi2101@gmail.com>
2018-04-25 12:24:53 +0300

    second commit

---
+++ lab1/Firma.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Firma
+    {
+        
+        
+        //свойства класса
+        public string Name { get; set; }         //свойство названия фирмы
+        public string Adres { get; set; }       //свойство адреса фирмы
+        public int Year { get; set; }          //свойство года основания фирмы
+        public Firma()  //конструктор по умолчанию
+        {
+            Name = null;
+            Adres = null;
+            Year = 0;
+        }
+        public virtual void Display() //метод для вывода 
+        {
+            Console.WriteLine("Название фирмы: " + Name);
+            Console.WriteLine("Адрес фирмы: " + Adres);
+            Console.WriteLine("Год основания: " + Year);
+        }
+        public virtual void Input()  //метод для ввода
+        {
+            Console.WriteLine("Название фирмы:  ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Адрес фирмы: ");
+            Adres = Console.ReadLine();
+            Console.WriteLine("Год основания: ");
+            Year = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
+
+    }
+}
